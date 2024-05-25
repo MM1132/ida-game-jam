5eb26aec5f8172591802403aefc39c6c226212a3
Alawode 'Fiyikewa <55203377+Itsjustkewa@users.noreply.github.com>
2024-05-26 02:44:55 +0300

    Update v3
    *Added More detailed art
    *Added the image looping

--- Assets/Scripts/Camera/BackgroundCameraLock.cs
+++ Assets/Scripts/Camera/BackgroundCameraLock.cs
@@ -6,29 +6,64 @@ using UnityEngine;
 public class BackgroundCameraLock : MonoBehaviour
 {
     //get the sprite render to get the world height of the image and clamp it to the camera
-    [SerializeField]Vector3 _startPos;
-    [SerializeField]float padding = 29f;
+    [SerializeField]float offset;
+    float spriteWidth,_camOthSize,visiableEdgeRight,visiableEdgeLeft;
     Camera _camera ;
-    [SerializeField]Vector3 _cam;
+    SpriteRenderer sptder;
+    Vector3 _cam;
+    [SerializeField]bool InvertScale;
+    [HideInInspector]public bool HasALeftBG,HasARightBG;
 
 
     void Awake()
     {
         _camera =FindFirstObjectByType <Camera>();
-        _startPos= GetComponent<RectTransform>().localPosition;
-    }
+        sptder = GetComponent<SpriteRenderer>();
+        spriteWidth = sptder.sprite.bounds.size.x;
+
+;    }
     void Update()
     {
-        /*
-        if(transform.localPosition.y < padding)
+        _camOthSize =_camera.orthographicSize * Screen.width/Screen.height;
+        visiableEdgeRight = (transform.localPosition.x + spriteWidth / 2) - _camOthSize;
+        visiableEdgeLeft = (transform.localPosition.x - spriteWidth / 2) + _camOthSize;
+        _cam =_camera.gameObject.GetComponent<Transform>().localPosition;
+        if(_cam.x >= visiableEdgeRight- offset&& !HasARightBG )
         {
+            MakeExtraBackground(1);
+            HasARightBG = true;
+        }else if (_cam.x<= visiableEdgeLeft + offset && !HasALeftBG)
+        {
+            MakeExtraBackground(-1);
+            HasALeftBG = true;
+        }
+
+        /*
+        //check if this current gameobject is still visible
+        if(!sptder.isVisible)
+        {
+            Debug.Log($"Name : {transform.gameObject.name}");
         }
         */
-        _cam =_camera.gameObject.GetComponent<Transform>().localPosition;
-        if(_camera.gameObject.GetComponent<Transform>().localPosition.x > padding)
+    }
+
+    void MakeExtraBackground(int rightOrLeft)
+    {
+        Vector3 newPosition = new Vector3(transform.localPosition.x+spriteWidth*rightOrLeft,transform.localPosition.y,transform.localPosition.z);
+        RectTransform newBg = Instantiate(transform,newPosition,transform.localRotation) as RectTransform;
+
+        if(InvertScale)
         {
-            Debug.Log("Should switch ");
-            //transform.localPosition = _startPos;
+            newBg.localScale = new Vector3(newBg.localScale.x *-1,newBg.localScale.y,newBg.localScale.z);  
+        }
+        newBg.SetParent(transform.parent);
+
+        if(rightOrLeft > 0)
+        {
+            newBg.GetComponent<BackgroundCameraLock>().HasALeftBG = true;
+        }else
+        {
+            newBg.GetComponent<BackgroundCameraLock>().HasARightBG = true;
         }
     }
 }
